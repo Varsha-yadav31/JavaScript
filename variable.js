c98f045991a2ae73f035c80081ab7ec85fe61898
Varsha-yadav31 <varshu31yadav@gmail.com>
2025-07-16 17:34:02 +0530

    variable.js

---
+++ variable.js
@@ -0,0 +1,51 @@
+// var is a block of code which store some value
+FullName = "Varsha";
+age = 20;
+console.log (FullName);
+console.log (age);
+// 
+x = null;// empty 
+y = undefined;//don't know 
+console.log (x);
+console.log (y);
+// there r 3 way to declare the variable 
+// 1. var = can be update and redeclare the value 
+// 2. const = can't redeclare or updated
+// 3. let = we can update the value but can't redeclared 
+ // as a good programmer we do not use var vcoz if we declare a vARIABLE BY VAR THEN WE CAN REDECLAre aur it is changeable 
+ // but if we decleare a variable by let 
+
+
+ //we can also defined in a curly bracket 
+
+ {let a = 5;
+    console.log(a);
+
+ }
+ {let a = 6 ;
+    console.log(a)
+ }
+  
+ // using const
+ const student = {
+    Fullname : "Varsha",
+    age : 20,
+    cgpa : 7.49,
+    isPass : true,
+
+ };
+ console.log(student);
+
+
+ // 
+ const Instaprofile = {
+    userName : "vxrsha2",
+    follower : 55,
+    following : 40,
+    post : 0,
+    
+
+ };
+ console.log(Instaprofile);
+ 
+
